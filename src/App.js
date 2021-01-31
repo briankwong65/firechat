9cedf7651aa8eef92c3af9e4c18509afb64ea72e
briankwong65 <briankwong98@gmail.com>
2021-01-31 15:59:24 +0800

    Create SignIn component

--- src/App.js
+++ src/App.js
@@ -21,12 +21,27 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
-  
+  const [user] = useAuthState(auth);
+
   return (
     <div className='App'>
       <header className='App-header'></header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
 }
 
 export default App;
