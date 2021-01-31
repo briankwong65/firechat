6be309aae9ff5651323092445afde65d2941af79
briankwong65 <briankwong98@gmail.com>
2021-01-31 15:55:04 +0800

    import firebase config

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firesto
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDhW1eBzXFfRbj5UMBd7BkAiBlVvMVJ7r0",
+  authDomain: "quick-chat-2a210.firebaseapp.com",
+  projectId: "quick-chat-2a210",
+  storageBucket: "quick-chat-2a210.appspot.com",
+  messagingSenderId: "958632476247",
+  appId: "1:958632476247:web:346bd06e8c92fc772c2d12",
+  measurementId: "G-7CMCER6ENC"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+  
   return (
     <div className='App'>
       <header className='App-header'></header>
