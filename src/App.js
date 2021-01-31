f06ebc3850aa4d200d0aec79a1c59f1bf4bcd558
briankwong65 <briankwong98@gmail.com>
2021-01-31 16:07:24 +0800

    Create ChatRoom component

--- src/App.js
+++ src/App.js
@@ -50,4 +50,19 @@ function SignOut() {
   )
 }
 
+function ChatRoom(){
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  return (
+    <>
+      <div>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+    </>
+  )
+}
+
 export default App;
