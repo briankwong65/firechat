274ca87c4d5444e8d99176eeb24aa1a2e76a6a21
briankwong65 <briankwong98@gmail.com>
2021-01-31 20:09:33 +0800

    Handle reinitialize app problem

--- src/App.js
+++ src/App.js
@@ -8,15 +8,19 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyDhW1eBzXFfRbj5UMBd7BkAiBlVvMVJ7r0',
-  authDomain: 'quick-chat-2a210.firebaseapp.com',
-  projectId: 'quick-chat-2a210',
-  storageBucket: 'quick-chat-2a210.appspot.com',
-  messagingSenderId: '958632476247',
-  appId: '1:958632476247:web:346bd06e8c92fc772c2d12',
-  measurementId: 'G-7CMCER6ENC',
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyDhW1eBzXFfRbj5UMBd7BkAiBlVvMVJ7r0',
+    authDomain: 'quick-chat-2a210.firebaseapp.com',
+    projectId: 'quick-chat-2a210',
+    storageBucket: 'quick-chat-2a210.appspot.com',
+    messagingSenderId: '958632476247',
+    appId: '1:958632476247:web:346bd06e8c92fc772c2d12',
+    measurementId: 'G-7CMCER6ENC',
+  });
+} else {
+  firebase.app(); // if already initialized, use that one
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -53,8 +57,13 @@ function ChatRoom() {
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
+  // console.log(query);
+  // console.log(messages);
+
   return (
     <>
+      <p>hello world</p>
+      <div>{messages}</div>
       <div>
         {messages &&
           messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
