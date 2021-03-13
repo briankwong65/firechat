33b97b8b4f0a5976669cf295224f77708e4871df
briankwong65 <briankwong98@gmail.com>
2021-03-13 19:42:18 +0800

    Add components folder

---
+++ src/components/SignIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+};
