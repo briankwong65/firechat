ce54885ec1d4a4ccd5a204db9cc6d54f2c234c17
briankwong65 <40626125+briankwong65@users.noreply.github.com>
2021-03-14 16:03:31 +0800

    Add submit button in ChatRoom to handle submission of text

--- src/components/ChatRoom/index.js
+++ src/components/ChatRoom/index.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as styles from './styles.module.scss';
 
+import firebase from 'firebase/app';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import ChatMessage from '../ChatMessage';
 
 const ChatRoom = (props) => {
-    const { firestore } = props;
+    const { firestore, auth } = props;
 
     const messagesRef = firestore.collection('messages');
     const query = messagesRef.orderBy('createdAt').limit(25);
     const [messages] = useCollectionData(query, { idField: 'id' });
 
+    const [formValue, setFormValue] = useState('');
+
+    const handleSubmit = (event) =>{
+        setFormValue(event.target.value);
+    }
+
+    const sendMessage = async(event) => {
+        event.preventDefault();
+        const { uid, photoURL } = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        });
+
+        setFormValue('');
+    }
+
     return (
         <>
-        <p>hello world</p>
-        <div>
-            {messages &&
-            messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-        </div>
+            <p>hello world</p>
+            <div>
+                {messages &&
+                messages.map((msg) => <ChatMessage key={msg.id} message={msg} auth={auth}/>)}
+            </div>
+            <form onSubmit={sendMessage}>
+                <input value={formValue} onChange={handleSubmit}/>
+                <button type='submit'>👾</button>
+            </form>
         </>
     );
 };
