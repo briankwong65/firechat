8a3e13cc98f2df0fa8f9e8547631cc68bd24c5e5
briankwong65 <40626125+briankwong65@users.noreply.github.com>
2021-03-14 19:14:20 +0800

    Update signOutBtn styles

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -5,7 +5,7 @@ const SignOut = (props) => {
     const { auth } = props;
 
     return (
-      auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+      auth.currentUser && <button className={styles.signOutBtn} onClick={() => auth.signOut()}>Sign Out</button>
     );
 };
 
