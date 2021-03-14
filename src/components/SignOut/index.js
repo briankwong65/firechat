b1513faade38690e21ceecdd677a725b430c53e1
briankwong65 <40626125+briankwong65@users.noreply.github.com>
2021-03-14 20:33:52 +0800

    Update signout btn

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import * as styles from './styles.module.scss';
 
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
 const SignOut = (props) => {
     const { auth } = props;
 
     return (
-      auth.currentUser && <button className={styles.signOutBtn} onClick={() => auth.signOut()}>Sign Out</button>
+      auth.currentUser && <ExitToAppIcon fontSize="large" className={styles.signOutBtn} onClick={() => auth.signOut()}/>
     );
 };
 
