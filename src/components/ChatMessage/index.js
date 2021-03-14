10859464d5cedbe770c553fc71439c89f90ecfc3
briankwong65 <40626125+briankwong65@users.noreply.github.com>
2021-03-14 20:33:22 +0800

    Add relative sent time for messages

--- src/components/ChatMessage/index.js
+++ src/components/ChatMessage/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import * as styles from './styles.module.scss';
 import classNames from 'classnames';
+import { fromUnixTime, formatRelative  } from 'date-fns';
 
 const ChatMessage = (props) => {
     const { auth, message } = props;
-    const { text, uid, photoURL } = message;
+    const { text, uid, photoURL, createdAt } = message;
+    const msgSentTime = createdAt && fromUnixTime(createdAt.seconds);
+    const relativeTime = msgSentTime && formatRelative(msgSentTime, new Date());
 
     return (
         uid === auth.currentUser.uid ?
         <div className={classNames(styles.message, styles.sent)}>
             <img className={styles.userIcon} src={photoURL} />
-            <div className={styles.text}>{text}</div>
+            <div className={styles.text}>{text}
+                <div className={styles.sentTime}>  {relativeTime}</div>
+            </div>
         </div>
         :
         <div className={classNames(styles.message, styles.received)}>
             <img className={styles.userIcon} src={photoURL} />
-            <div className={styles.text}>{text}</div>
+            <div className={styles.text}>{text}
+                <div className={styles.sentTime}>{relativeTime}</div>
+            </div>
         </div>
     );
 };
