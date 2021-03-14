0d7f645d74110757433bc19118ecd9d05148123e
briankwong65 <40626125+briankwong65@users.noreply.github.com>
2021-03-14 16:03:01 +0800

    Update the ChatMessage to show photo of the user and add class for sender and receiver

--- src/components/ChatMessage/index.js
+++ src/components/ChatMessage/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import * as styles from './styles.module.scss';
 
 const ChatMessage = (props) => {
-    const { text, uid } = props.message;
-    return <p>{text}</p>;
+    const { auth, message } = props;
+    const { text, uid, photoURL } = message;
+
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+    return (
+        <div className={`message ${messageClass}`}>
+            <img src={photoURL} />
+            <p>{text}</p>
+        </div>
+    );
 };
 
 export default ChatMessage;
