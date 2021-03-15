bda39186804ea2eb56a0f6bb964136a5c3367324
briankwong65 <briankwong98@gmail.com>
2021-03-16 00:36:18 +0800

    Extract firebaseConfig to an external json file

--- src/App.js
+++ src/App.js
@@ -5,29 +5,20 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import firebaseConfig from './firebaseConfig.json';
 
 import SignIn from './components/SignIn';
 import ChatRoom from './components/ChatRoom';
 import SignOut from './components/SignOut';
 
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: 'AIzaSyDhW1eBzXFfRbj5UMBd7BkAiBlVvMVJ7r0',
-    authDomain: 'quick-chat-2a210.firebaseapp.com',
-    projectId: 'quick-chat-2a210',
-    storageBucket: 'quick-chat-2a210.appspot.com',
-    messagingSenderId: '958632476247',
-    appId: '1:958632476247:web:346bd06e8c92fc772c2d12',
-    measurementId: 'G-7CMCER6ENC',
-  });
+  firebase.initializeApp(firebaseConfig);
 } else {
   firebase.app(); // if already initialized, use that one
 }
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-
-
 
 const App = () => {
   const [user] = useAuthState(auth);
@@ -36,9 +27,15 @@ const App = () => {
     <div className='App'>
       <header>
         <h1>🔥Firechat</h1>
-        <SignOut auth={auth}/>
+        <SignOut auth={auth} />
       </header>
-      <section>{user ? <ChatRoom firestore={firestore} auth={auth}/> : <SignIn auth={auth} />}</section>
+      <section>
+        {user ? (
+          <ChatRoom firestore={firestore} auth={auth} />
+        ) : (
+          <SignIn auth={auth} />
+        )}
+      </section>
     </div>
   );
 };
